63b949f416c6e8b341099141035e8a1af0203535
Abraham-Saindou <abraham.saindou@laplateforme.io>
2023-06-26 01:21:37 +0200

    untrack commit

---
+++ cmake-build-debug/CMakeFiles/Distributeur.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Distributeur.dir/src/Database.cpp.o"
+  "CMakeFiles/Distributeur.dir/src/Database.cpp.o.d"
+  "CMakeFiles/Distributeur.dir/src/main.cpp.o"
+  "CMakeFiles/Distributeur.dir/src/main.cpp.o.d"
+  "Distributeur"
+  "Distributeur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Distributeur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
